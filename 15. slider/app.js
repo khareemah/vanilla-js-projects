093283120a5dc4c7a95343b9561ee361022ab714
khareemah <ajimobikareemah@gmail.com>
2021-02-18 19:56:26 -0800

    add min-height to sections

--- 15. slider/app.js
+++ 15. slider/app.js
@@ -2,3 +2,13 @@ const slides = document.querySelectorAll(".slider");
 slides.forEach((slide, index) => {
   slide.style.left = `${100 * index}%`;
 });
+
+const prev = document.querySelector("button.prev");
+const next = document.querySelector("button.next");
+let counter = 0;
+next.addEventListener("click", function() {
+  counter++;
+  slides.forEach(slide => {
+    slide.style.transform = `translateX(-${100 * counter})%`;
+  });
+});
